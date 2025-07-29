89a6eb9fd856aacd3493cf9c000ebed45180d767
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-07-30 01:48:53 +0900

    TopPageのリファクタリング

--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -1,34 +1,20 @@
-
+import { NotLoginLayout }  from '../../components/templates/NotLoginLayout'
 import PrimaryBtn  from '../../components/atoms/PrimaryBtn'
 
 export function TopPage(){
     return(
-        <div className="relative">
-            <header className="fixed bg-white leading-[80px] top-0 right-0 left-0 flex flex-col items-center justify-center">
-                <div className="mx-auto container flex justify-between">
-                    <p className="logo">スケジュール管理App</p>
-                    <nav>
-                        <ul className="flex gap-5 font-bold text-sky-400">
-                            <li className="hover:text-sky-700 hover:underline transition">利用説明</li>
-                            <li className="hover:text-sky-700 hover:underline transition">新規登録</li>
-                            <li className="hover:text-sky-700 hover:underline transition">ログイン</li>
-                        </ul>
-                    </nav>
-                </div>
-            </header>
-            <main className="bg-gradient-to-r from-sky-400 to-sky-500 flex flex-col items-center justify-center h-screen">
-                <div className="text-center">
-                    <h1 className="logo text-7xl">スケジュール管理</h1>
-                    <p className="pt-[30px] font-bold text-3xl">このアプリは、スケジュールを管理するためのアプリです。</p>
-                    <p className="pt-[30px] font-bold text-3xl">ユーザーは、スケジュールを登録、編集、削除することができます。</p>
-                    <p className="pt-[30px] font-bold text-3xl">また、スケジュールをカレンダー形式で表示することができます。</p>
-                </div>
-                <div className="pt-[20vh]">
-                    <PrimaryBtn>
-                        ログイン
-                    </PrimaryBtn>
-                </div>
-            </main>
-        </div>
+        <NotLoginLayout>
+            <div className="text-center">
+                <h1 className="logo text-7xl">スケジュール管理</h1>
+                <p className="pt-[30px] font-bold text-3xl">このアプリは、スケジュールを管理するためのアプリです。</p>
+                <p className="pt-[30px] font-bold text-3xl">ユーザーは、スケジュールを登録、編集、削除することができます。</p>
+                <p className="pt-[30px] font-bold text-3xl">また、スケジュールをカレンダー形式で表示することができます。</p>
+            </div>
+            <div className="pt-[20vh]">
+                <PrimaryBtn onClick= {() => null}>
+                    ログイン
+                </PrimaryBtn>
+            </div>
+        </NotLoginLayout>
     )
 }
