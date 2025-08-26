255b7c3ddea49632a9d42d0a2997d42c5cde0c5e
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-08-27 00:46:16 +0900

    ログインボタンをカタログ化

---
+++ src/stories/LoginButton.stories.tsx
@@ -0,0 +1,11 @@
+import LoginButton from '../components/atoms/LoginButton';
+
+const meta = {
+  id: 'atoms-loginbutton',
+  title: 'Atoms/LoginButton',
+  component: LoginButton,
+  args: { href: '/login', children: 'ログイン' },
+} satisfies Meta<typeof LoginButton>;
+export default meta;
+type Story = StoryObj<typeof meta>;
+export const Default: Story = {};
